8a9e32169ee3e8ce237c0682ac6004a46f880b30
rainfishs <rainfish122456@gmail.com>
2024-10-24 16:02:02 +0800

    modify main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    def __init__(self, name: str, price: float, description: str = None):
+        self.name = name
+        self.price = price
+        self.description = description
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/items/")
+def create_item(item: Item):
+    print(f'received item: {item}')
+    return {"item_received": item}
