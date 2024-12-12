5b5fb8e475414c94e36353e3f4c745b96530e308
rainfishs <rainfish122456@gmail.com>
2024-12-12 15:21:37 +0800

    Fixed database

--- app/database.py
+++ app/database.py
@@ -9,8 +9,5 @@ engine = create_engine(DATABASE_URL)
 # Define Tables(Models)
 Base = declarative_base()
 
-# Initialize Database's Table
-Base.metadata.create_all(bind=engine)
-
 # factory method
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+from .database import Base, engine
 from .routers import todos_router
 
 app = FastAPI()
+
+# Initialize Database's Table
+Base.metadata.create_all(bind=engine)
 
 app.include_router(todos_router, prefix="/api")
