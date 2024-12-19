41b9215eedae825565ecf0ec3a7de095fddcf448
rainfishs <rainfish122456@gmail.com>
2024-12-19 15:03:52 +0800

    Upgrade Todos

--- app/models.py
+++ app/models.py
@@ -14,6 +14,8 @@ class Todo(Base):
     description = Column(String, nullable=True)
     completed = Column(Boolean, default=False)
     due_date = Column(Date, nullable=True)
+    priority = Column(Integer, default=1)
+
 
 class User(Base):
     __tablename__ = "users"
