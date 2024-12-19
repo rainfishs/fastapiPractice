41a9b0eac48cf7a105466a39bea53514bccbf9e2
rainfishs <rainfish122456@gmail.com>
2024-12-19 15:03:25 +0800

    Fixed alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,14 +1,10 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
-import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__).resolve().parents[1]))
-from app.main import Base
+from sqlalchemy import engine_from_config, pool
 
 from alembic import context
+from app.main import Base
+from app.models import Todo
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -69,9 +65,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
--- app/main.py
+++ app/main.py
@@ -6,6 +6,6 @@ from .routers import todos_router
 app = FastAPI()
 
 # Initialize Database's Table
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 app.include_router(todos_router, prefix="/api")
